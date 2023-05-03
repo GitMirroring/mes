7b270330546beb0fdbf6d445ff5a5eef85cb6e3c
Janneke Nieuwenhuizen <janneke@gnu.org>
2023-05-03 18:43:08 +0200

    Revert "core: Remove duplicate environment initialization."
    This reverts commit 18af66f4170998f7e548e4e40a4bf73585bf01ae.

--- src/mes.c
+++ src/mes.c
@@ -1,6 +1,6 @@
 /* -*-comment-start: "//";comment-end:""-*-
  * GNU Mes --- Maxwell Equations of Software
- * Copyright © 2016,2017,2018,2019,2020,2021,2023 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+ * Copyright © 2016,2017,2018,2019,2020,2021 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
  *
  * This file is part of GNU Mes.
  *
@@ -157,8 +157,9 @@ read_boot ()                    /*:((internal)) */
 }
 
 void
-init ()
+init (char **envp)
 {
+  environ = envp;
   __execl_c_argv = malloc (1024 * sizeof (char *));     /* POSIX minimum: 4096 */
   __gettimeofday_time = malloc (sizeof (struct timeval));
   __get_internal_run_time_ts = malloc (sizeof (struct timespec));
@@ -181,9 +182,9 @@ init ()
 }
 
 int
-main (int argc, char **argv)
+main (int argc, char **argv, char **envp)
 {
-  init ();
+  init (envp);
 
   struct scm *a = mes_environment (argc, argv);
   a = mes_builtins (a);
