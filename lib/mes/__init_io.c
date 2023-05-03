ec37dbada38a835345156700fa2d056448294025
Janneke Nieuwenhuizen <janneke@gnu.org>
2023-05-03 12:29:22 +0200

    squash! DRAFT riscv64: Cater for M2-Planet.
    * lib/mes/__init_io.c (__init_io): Call main to avoid messing up
    parameters.

--- lib/mes/__init_io.c
+++ lib/mes/__init_io.c
@@ -20,11 +20,12 @@
 
 #include "mes/lib-mini.h"
 
-void
+int
 __init_io (int argc, char** argv, char** envp)
 {
   __stdin = 0;
   __stdout = 1;
   __stderr = 2;
   environ = envp;
+  return main (argc, argv, envp);
 }
