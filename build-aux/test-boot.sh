d487c4a12b0f8d5fd658c436afb52c799e73cf03
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-04-21 18:05:41 +0200

    build: Use bash for files with bashisms.
    Reported by deesix via #bootstrappable.
    
    * build-aux/test-boot.sh,
    build-aux/test-c.sh: Use /usr/bin/env bash.

--- build-aux/test-boot.sh
+++ build-aux/test-boot.sh
@@ -1,7 +1,7 @@
-#! /bin/sh
+#! /usr/bin/env bash
 
 # GNU Mes --- Maxwell Equations of Software
-# Copyright © 2018,2019 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+# Copyright © 2018,2019,2025 Janneke Nieuwenhuizen <janneke@gnu.org>
 #
 # This file is part of GNU Mes.
 #
