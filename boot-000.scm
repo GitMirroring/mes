9bb4188487f31a23abf09020283185e864acb6c1
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-07-27 12:36:30 +0200

    Revert "WIP fresh macro_expand without eval"
    This reverts commit 42faf4401b5745d7cf9aa3ff623d003f6a559529.

--- boot-000.scm
+++ boot-000.scm
@@ -1,7 +1,5 @@
 ;; make -f simple.make && MES_DEBUG=5 MES_BOOT=boot-000.scm ./pre-inst-env mes-gcc
 
-(define (primitive-eval e) (core:eval e (current-environment)))
-
 (define-macro (when expr . body)
   (list 'if expr
         (list (append2 (list 'lambda ()) body))))
