e2b05aeb5bceca2cbada982d16b8906dd1cd8a7d
Stefan <stefan-guix@vodafonemail.de>
2025-09-21 15:57:25 +0200

    DRAFT lib: tests: Add 87-sizeof-union.
    * lib/tests/scaffold/87-sizeof-union.c: New test.
    * build-aux/check-mescc.sh (tcc_tests): Register it.
    
    Co-authored-by: Janneke Nieuwenhuizen <janneke@gnu.org>

--- build-aux/check-mescc.sh
+++ build-aux/check-mescc.sh
@@ -225,6 +225,7 @@ lib/tests/scaffold/87-nested-cond-expression.c
 lib/tests/scaffold/87-sizeof-tag.c
 lib/tests/scaffold/87-struct-tag-pointer.c
 lib/tests/scaffold/87-string-element-type.c
+lib/tests/scaffold/87-sizeof-union.c
 "
 
 gnu_tests="
