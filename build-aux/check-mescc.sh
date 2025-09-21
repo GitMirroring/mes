fa32af189992654b6c10f6be743f1d9387b3c081
Stefan <stefan-guix@vodafonemail.de>
2025-09-21 20:43:45 +0200

    DRAFT lib: tests: Add 87-init-struct7.
    * lib/tests/scaffold/87-init-struct7.c: New test.
    * build-aux/check-mescc.sh (tcc_tests): Register it.
    
    Co-authored-by: Janneke Nieuwenhuizen <janneke@gnu.org>

--- build-aux/check-mescc.sh
+++ build-aux/check-mescc.sh
@@ -246,6 +246,7 @@ lib/tests/scaffold/87-init-struct4.c
 lib/tests/scaffold/87-init-struct5.c
 lib/tests/scaffold/87-sizeof-initialized-array.c
 lib/tests/scaffold/87-init-struct6.c
+lib/tests/scaffold/87-init-struct7.c
 "
 
 gnu_tests="
