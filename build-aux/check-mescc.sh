661a3f2bbed944886092c914477e3b0de6fe64bf
Stefan <stefan-guix@vodafonemail.de>
2025-09-21 17:20:15 +0200

    DRAFT lib: tests: Add 87-cond-expression-expression.
    * lib/tests/scaffold/87-cond-expression-expression.c: New test.
    * build-aux/check-mescc.sh (tcc_tests): Register it.
    
    Co-authored-by: Janneke Nieuwenhuizen <janneke@gnu.org>

--- build-aux/check-mescc.sh
+++ build-aux/check-mescc.sh
@@ -231,6 +231,7 @@ lib/tests/scaffold/87-cast-function-pointer.c
 lib/tests/scaffold/87-multiple-comma-operators.c
 lib/tests/scaffold/87-lookup-tag.c
 lib/tests/scaffold/87-call-function-pointer-cast.c
+lib/tests/scaffold/87-cond-expression-expression.c
 "
 
 gnu_tests="
