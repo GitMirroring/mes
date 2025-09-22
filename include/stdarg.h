9427335351e2447d03515d0880775973ace094b8
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-09-22 12:18:47 +0200

    DRAFT lib: Move v*printf v*scanf prototypes to stdio.h.
    * include/stdarg.h (vfprintf, vfscanf, vprintf, vsnprintf, vsprintf,
    vsscanf): Move to...
    * include/stdio.h (vfprintf, vfscanf, vprintf, vsnprintf, vsprintf,
    vsscanf): ...here.
    * lib/stdio/vsnprintf.c: Add missing stdio.h include.
    * lib/stdio/vsprintf.c: Likewise.

--- include/stdarg.h
+++ include/stdarg.h
@@ -82,12 +82,6 @@ typedef char *va_list;
 #define va_copy(dest, src) dest = src
 
 int vexec (char const *file_name, va_list ap);
-int vfprintf (FILE * stream, char const *template, va_list ap);
-int vfscanf (FILE * stream, char const *template, va_list ap);
-int vprintf (char const *format, va_list ap);
-int vsprintf (char *str, char const *format, va_list ap);
-int vsnprintf (char *str, size_t size, char const *format, va_list ap);
-int vsscanf (char const *s, char const *template, va_list ap);
 
 #endif // ! SYSTEM_LIBC
 
