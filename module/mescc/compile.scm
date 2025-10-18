79cccd898e8cb071b2494eafb913530b78ea07bb
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-10-18 09:03:07 +0200

    DRAFT mescc: Simplify lshift/rshift handling.
    From <https://en.cppreference.com/w/c/language/operator_arithmetic.html> regarding “Shift operators”:
    
        “First, integer promotions are performed, individually, on each
        operand (Note: this is unlike other binary arithmetic operators,
        which all perform usual arithmetic conversions).  The type of the
        result is the type of lhs after promotion."
    
    We use binop->r, which does that arithmetic conversion.  But that's not
    a problem, if we convert the result afterwards.
    
    * module/mescc/compile.scm (expr->register)[lshift,rshift]: Simplify.
    
    Co-authored-by: Stefan <stefan-guix@vodafonemail.de>

--- module/mescc/compile.scm
+++ module/mescc/compile.scm
@@ -1251,28 +1251,14 @@
         ((bitwise-or ,a ,b) ((binop->r info) a b 'r0-or-r1))
         ((bitwise-xor ,a ,b) ((binop->r info) a b 'r0-xor-r1))
         ((lshift ,a ,b)
-         (let* ((type-a (ast->type a info))
-                (default (get-type "default" info))
-                ;; In helper of expr->register in the matches for lshift and
-                ;; rshift the usual arithmetic conversion is wrong.  A quick and
-                ;; dirty fix is to use the type of the operand 'a' if it has a
-                ;; greater or equal size than the default type.
-                ;; TODO: The type of the result is that of lhs.
-                (type (if (>= (->size type-a info) (->size default info)) type-a
-                          default))
+         (let* ((type (promote a info))
                 (info ((binop->r info) a b 'r0<<r1)))
            (append-text info (convert-r0 info type))))
         ((rshift ,a ,b)
-         (let* ((type-a (ast->type a info))
-                (default (get-type "default" info))
-                ;; In helper of expr->register in the matches for lshift and
-                ;; rshift the usual arithmetic conversion is wrong.  A quick and
-                ;; dirty fix is to use the type of the operand 'a' if it has a
-                ;; greater or equal size than the default type.
-                ;; TODO: The type of the result is that of lhs.
-                (type (if (>= (->size type-a info) (->size default info)) type-a
-                          default))
-                (info ((binop->r info) a b (if (signed? type) 'r0>>r1-signed 'r0>>r1))))
+         (let* ((type (promote a info))
+                (info ((binop->r info) a b
+                       (if (signed? type) 'r0>>r1-signed
+                           'r0>>r1))))
            (append-text info (convert-r0 info type))))
         ((div ,a ,b)
          ((binop->r info) a b 'r0/r1
